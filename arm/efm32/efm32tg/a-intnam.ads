3a49cb2d47baafdce0b11fcafa1e4114ab66c41f
Eckhart Kőppen <eck@40hz.org>
2017-12-11 22:29:07 +0200

    Update EFM32 IRQs

--- arm/efm32/efm32tg/a-intnam.ads
+++ arm/efm32/efm32tg/a-intnam.ads
@@ -1,6 +1,10 @@
 --
 --  Copyright (C) 2017, AdaCore
 --
+
+--  This spec has been automatically generated from EFM32TG842F32.svd
+
+--  This is a version for the Silicon Labs EFM32TG842F32 Cortex-M MCU MCU
 package Ada.Interrupts.Names is
 
    --  All identifiers in this unit are implementation defined
@@ -11,29 +15,30 @@ package Ada.Interrupts.Names is
    -- Interrupts --
    ----------------
 
-   Sys_Tick_Interrupt    : constant Interrupt_ID := -1;
-   DMA_Interrupt         : constant Interrupt_ID := 0;
-   GPIO_EVEN_Interrupt   : constant Interrupt_ID := 1;
-   TIMER0_Interrupt      : constant Interrupt_ID := 2;
-   USART0_RX_Interrupt   : constant Interrupt_ID := 3;
-   USART0_TX_Interrupt   : constant Interrupt_ID := 4;
-   ACMP0_ACMP1_Interrupt : constant Interrupt_ID := 5;
-   ADC0_Interrupt        : constant Interrupt_ID := 6;
-   DAC0_Interrupt        : constant Interrupt_ID := 7;
-   I2C0_Interrupt        : constant Interrupt_ID := 8;
-   GPIO_ODD_Interrupt    : constant Interrupt_ID := 9;
-   TIMER1_Interrupt      : constant Interrupt_ID := 10;
-   USART1_RX_Interrupt   : constant Interrupt_ID := 11;
-   USART1_TX_Interrupt   : constant Interrupt_ID := 12;
-   LESENSE_Interrupt     : constant Interrupt_ID := 13;
-   LEUART0_Interrupt     : constant Interrupt_ID := 14;
-   LETIMER0_Interrupt    : constant Interrupt_ID := 15;
-   PCNT0_Interrupt       : constant Interrupt_ID := 16;
-   RTC_Interrupt         : constant Interrupt_ID := 17;
-   CMU_Interrupt         : constant Interrupt_ID := 18;
-   VCMP_Interrupt        : constant Interrupt_ID := 19;
-   LCD_Interrupt         : constant Interrupt_ID := 20;
-   MSC_Interrupt         : constant Interrupt_ID := 21;
-   AES_Interrupt         : constant Interrupt_ID := 22;
+   --  System tick
+   Sys_Tick             : constant Interrupt_ID := -1;
+   DMA                  : constant Interrupt_ID := 0;
+   GPIO_EVEN            : constant Interrupt_ID := 1;
+   TIMER0               : constant Interrupt_ID := 2;
+   USART0_RX            : constant Interrupt_ID := 3;
+   USART0_TX            : constant Interrupt_ID := 4;
+   ACMP0                : constant Interrupt_ID := 5;
+   ADC0                 : constant Interrupt_ID := 6;
+   DAC0                 : constant Interrupt_ID := 7;
+   I2C0                 : constant Interrupt_ID := 8;
+   GPIO_ODD             : constant Interrupt_ID := 9;
+   TIMER1               : constant Interrupt_ID := 10;
+   USART1_RX            : constant Interrupt_ID := 11;
+   USART1_TX            : constant Interrupt_ID := 12;
+   LESENSE              : constant Interrupt_ID := 13;
+   LEUART0              : constant Interrupt_ID := 14;
+   LETIMER0             : constant Interrupt_ID := 15;
+   PCNT0                : constant Interrupt_ID := 16;
+   RTC                  : constant Interrupt_ID := 17;
+   CMU                  : constant Interrupt_ID := 18;
+   VCMP                 : constant Interrupt_ID := 19;
+   LCD                  : constant Interrupt_ID := 20;
+   MSC                  : constant Interrupt_ID := 21;
+   AES                  : constant Interrupt_ID := 22;
 
 end Ada.Interrupts.Names;
